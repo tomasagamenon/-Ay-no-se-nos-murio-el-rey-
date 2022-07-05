438b174474fcaf0bce4ec0ebec16c14f2c6a1e21
tomasagamenon <tomas.agamenon@gmail.com>
2022-07-05 17:37:58 -0300

    Mala mia brother

---
+++ ¡Ay no, se nos murio el rey!/Assets/Scripts/Multiplayer/Manager/GameSettings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Manager/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    [SerializeField]
+    private string _gameVersion = "0.0.0";
+    public string GameVersion { get { return _gameVersion; } }
+    [SerializeField]
+    private string _nickName = "Kodak";
+    public string NickName 
+    { 
+        get
+        {
+            int value = Random.Range(0, 9999);
+            string valueString;
+            if (value < 10)
+                valueString = "000" + value.ToString();
+            else if (value < 100)
+                valueString = "00" + value.ToString();
+            else if (value < 1000)
+                valueString = "0" + value.ToString();
+            else valueString = value.ToString();
+            return _nickName + "#" + valueString;
+        }
+    }
+}
